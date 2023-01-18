1b8ff6963512e724d9d4e0e597e9bb912f747219
Cameron <cameron.studdstreet@gmail.com>
2023-01-18 08:21:04 +0000

    0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,10 @@
 //! }
 //!
 //! impl Validate for PhoneNumber {
+//!   type Context<'a> = ();
 //!   type Error = InvalidPhoneNumber;
 //!
-//!   fn is_valid(&self) -> Result<(), Self::Error> {
+//!   fn is_valid(&self, _ctx: Self::Context<'_>) -> Result<(), Self::Error> {
 //!     if self.0.len() == 11 {
 //!         return Err(InvalidPhoneNumber::WrongLength);
 //!     }
@@ -41,21 +42,112 @@
 //!   println!("This is a definitely valid phone number: {:?}", number.into_inner());
 //! }
 //! ```
+//!
+//! Some validation requires access to some context. For example, you may want to validate that an
+//! email address exists in your database. For that, you can pass this context via the `Context`
+//! associated type.
+//!
+//! When validating, you can call `foo.validate_with(context)`:
+//! ```
+//! # use validity::*;
+//! # struct Database {}
+//! # impl Database {
+//! #   fn check_phone_exists(&self, _: &PhoneNumber) -> bool { true }
+//! #   fn new() -> Self { Self {} }
+//! # }
+//! #[derive(Debug)]
+//! struct PhoneNumber(String);
+//!
+//! enum InvalidPhoneNumber {
+//!   NonDigit,
+//!   WrongLength,
+//!   NotInDatabase,
+//! }
+//!
+//! impl Validate for PhoneNumber {
+//!   type Context<'a> = Database;
+//!   type Error = InvalidPhoneNumber;
+//!
+//!   fn is_valid(&self, db: Self::Context<'_>) -> Result<(), Self::Error> {
+//!     if self.0.len() == 11 {
+//!         return Err(InvalidPhoneNumber::WrongLength);
+//!     }
+//!
+//!     if self.0.chars().any(|c| !c.is_digit(10)) {
+//!         return Err(InvalidPhoneNumber::NonDigit);
+//!     }
+//!
+//!     if !db.check_phone_exists(self) {
+//!         return Err(InvalidPhoneNumber::NotInDatabase);
+//!     }
+//!
+//!     Ok(())
+//!   }
+//! }
+//! ```
+//! You can then call this with:
+//! ```rust
+//! # use validity::*;
+//! # struct Database {}
+//! # impl Database {
+//! #   fn check_phone_exists(&self, _: &PhoneNumber) -> bool { true }
+//! #   fn new() -> Self { Self {} }
+//! # }
+//! # #[derive(Debug)]
+//! # struct PhoneNumber(String);
+//! # enum InvalidPhoneNumber {
+//! #   NonDigit,
+//! #   WrongLength,
+//! #   NotInDatabase,
+//! # }
+//! # impl Validate for PhoneNumber {
+//! #   type Context<'a> = Database;
+//! #   type Error = InvalidPhoneNumber;
+//! #   fn is_valid(&self, db: Self::Context<'_>) -> Result<(), Self::Error> {
+//! #     if self.0.len() == 11 {
+//! #         return Err(InvalidPhoneNumber::WrongLength);
+//! #     }
+//! #     if self.0.chars().any(|c| !c.is_digit(10)) {
+//! #         return Err(InvalidPhoneNumber::NonDigit);
+//! #     }
+//! #     if !db.check_phone_exists(self) {
+//! #         return Err(InvalidPhoneNumber::NotInDatabase);
+//! #     }
+//! #     Ok(())
+//! #   }
+//! # }
+//! let db = Database::new();
+//! let phone = PhoneNumber("01234567890".to_string());
+//! phone.validate_with(db);
+//! ```
 
-use std::ops::Deref;
+use core::ops::Deref;
 
 /// A thin wrapper around a value that guarantees that it is "valid"
 ///
 /// A `Valid<T>` can only be constructed by calling [`Validate::validate`] and then handling the
 /// possible error
+///
+/// Note, `Valid<T>` is not `repr(transparent)`, so using `transmute` to forcibly convert is
+/// undefined behaviour.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(transparent)]
 pub struct Valid<T>(T);
 
 impl<T> Valid<T> {
     /// Consume self and return the inner value
     pub fn into_inner(self) -> T {
         self.0
+    }
+
+    /// Create a `Valid<T>` without validating
+    ///
+    /// This is only available with the `test-mock` feature enabled.
+    ///
+    /// It goes without saying that this function invalidates all compile-time guarantees. It's
+    /// provided as an "escape hatch", intended for testing. While 
+    #[cfg(feature = "test-mock")]
+    pub fn danger_new_unvalidated(t: T) -> Self {
+        Self(t)
     }
 }
 
@@ -83,9 +175,10 @@ impl<T> Deref for Valid<T> {
 /// }
 ///
 /// impl Validate for PhoneNumber {
+///   type Context<'a> = ();
 ///   type Error = InvalidPhoneNumber;
 ///
-///   fn is_valid(&self) -> Result<(), Self::Error> {
+///   fn is_valid(&self, _ctx: Self::Context<'_>) -> Result<(), Self::Error> {
 ///     if self.0.len() == 11 {
 ///         return Err(InvalidPhoneNumber::WrongLength);
 ///     }
@@ -112,6 +205,9 @@ impl<T> Deref for Valid<T> {
 /// }
 /// ```
 pub trait Validate {
+    /// Context required for validation
+    type Context<'a>;
+
     /// The error returned by validation operations
     type Error;
 
@@ -119,7 +215,18 @@ pub trait Validate {
     ///
     /// Valid data should return `Ok(())`, and invalid data should return `Err(Self::Error)` which
     /// indicates the reason why validation failed
-    fn is_valid(&self) -> Result<(), Self::Error>;
+    fn is_valid(&self, ctx: Self::Context<'_>) -> Result<(), Self::Error>;
+
+    /// Validate with the given context
+    fn validate_with(self, ctx: Self::Context<'_>) -> Result<Valid<Self>, Self::Error>
+    where
+        Self: Sized,
+    {
+        match self.is_valid(ctx) {
+            Ok(()) => Ok(Valid(self)),
+            Err(e) => Err(e),
+        }
+    }
 
     /// Validate this object, and if successful return a `Valid<Self>` which acts as a "proof of
     /// validity"
@@ -127,12 +234,9 @@ pub trait Validate {
     /// If validation fails, the error is propagated upwards
     fn validate(self) -> Result<Valid<Self>, Self::Error>
     where
+        Self: for<'a> Validate<Context<'a> = ()>,
         Self: Sized,
     {
-        match self.is_valid() {
-            Ok(()) => Ok(Valid(self)),
-            Err(e) => Err(e),
-        }
+        self.validate_with(())
     }
 }
-
